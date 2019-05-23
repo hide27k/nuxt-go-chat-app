d293182f6dfca2940f7f20f425e9c484099b8e44
hideUW <hidehusky1996@gmail.com>
2019-05-23 15:49:10 -0700

    Add NoSuchDataError

--- server/domain/model/errors.go
+++ server/domain/model/errors.go
@@ -1,5 +1,22 @@
 package model
 
+import "fmt"
+
+// NoSuchDataError represents that spesific data doesn't exist.
+type NoSuchDataError struct {
+	BaseErr error
+	PropertyNameForDeveloper
+	PropertyNameForUser
+	PropertyValue interface{}
+	DomainModelNameForDeveloper
+	DomainModelNameForUser
+}
+
+// Error returns error message.
+func (e *NoSuchDataError) Error() string {
+	return fmt.Sprintf("no such data, %s: %v, %s", e.PropertyNameForDeveloper, e.PropertyValue, e.DomainModelNameForDeveloper)
+}
+
 // SQLError means SQL error.
 type SQLError struct {
 	BaseErr                   error
