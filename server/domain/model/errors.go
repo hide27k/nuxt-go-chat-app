24c17315b90941d71670ae18f13188dc63472e94
hideUW <hidehusky1996@gmail.com>
2019-05-23 23:25:27 -0700

    Add RepositoryMethod.

--- server/domain/model/errors.go
+++ server/domain/model/errors.go
@@ -2,6 +2,18 @@ package model
 
 import "fmt"
 
+// RepositoryMethod define the methods of repository.
+type RepositoryMethod string
+
+// Method of Repository
+const (
+	RepositoryMethodREAD   = "READ"
+	RepositoryMethodINSERT = "INSERT"
+	RepositoryMethodUPDATE = "UPDATE"
+	RepositoryMethodDELETE = "DELETE"
+	RepositoryMethodLIST   = "LIST"
+)
+
 // NoSuchDataError represents that spesific data doesn't exist.
 type NoSuchDataError struct {
 	BaseErr error
@@ -15,6 +27,19 @@ type NoSuchDataError struct {
 // Error returns error message.
 func (e *NoSuchDataError) Error() string {
 	return fmt.Sprintf("no such data, %s: %v, %s", e.PropertyNameForDeveloper, e.PropertyValue, e.DomainModelNameForDeveloper)
+}
+
+// RepositoryError represents error related to repository.
+type RepositoryError struct {
+	BaseErr          error
+	RepositoryMethod RepositoryMethod
+	DomainModelNameForDeveloper
+	DomainModelNameForUser
+}
+
+// Error returs error messages base on the given RepositoryError
+func (e *RepositoryError) Error() string {
+	return fmt.Sprintf("failed Repository operation, %s, %s", e.RepositoryMethod, e.DomainModelNameForDeveloper)
 }
 
 // SQLError means SQL error.
